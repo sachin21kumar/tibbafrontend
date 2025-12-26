6c02e94ddf89c60b5d0a1660756b910585dc1481
vinay <vinaykumar.mrski@gmail.com>
2025-12-26 18:36:07 +0530

    build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import MenuPage from "./components/products/getAllProducts";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen  justify-center bg-zinc-50 font-sans dark:bg-black">
-    <MenuPage/>
+    <Suspense fallback={<p>Loading...</p>}>
+      <MenuPage />
+    </Suspense>
     </div>
   );
 }
