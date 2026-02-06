76a2420508861c119ef9ec46c535111a3ce75f8b
vinay <vinaykumar.mrski@gmail.com>
2026-02-06 15:31:31 +0530

    dmoain

--- src/app/admin/order/page.tsx
+++ src/app/admin/order/page.tsx
@@ -1,5 +1,10 @@
 import AdminOrdersPage from "@/app/components/admin/order/Order";
 
 export default function index() {
-  return <AdminOrdersPage />;
+  return (
+    <div className="h-[54vh] flex justify-center items-center">
+      <p className="!font-[system-ui] text-xl">Coming Soon</p>
+      {/* <AdminOrdersPage /> */}
+    </div>
+  );
 }
