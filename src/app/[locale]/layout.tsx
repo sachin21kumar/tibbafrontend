51b0b88f758697f8707617cf238a7b75b16bc2d0
sachin21kumar <developer.mrski@gmail.com>
2026-08-06 15:38:41 +0530

    gtm tag edit

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -41,15 +41,15 @@ export default async function RootLayout({
     <html lang={locale} dir="ltr">
       <head>
         {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P8B889BZ');`,
-          }}
-        />
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P8B889BZ');
+          `}
+        </Script>
         {/* End Google Tag Manager */}
 
         {/* Google tag (gtag.js) - Analytics */}
